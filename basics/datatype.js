c332d646cb72f1aa038f2d391b7def8765240d5a
anshul0112 <sharmaanshul4418@gmail.com>
2025-01-31 10:19:55 +0000

    datatype

---
+++ basics/datatype.js
@@ -0,0 +1,22 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3) //we are using node js not browser
+
+console.log(3+3)
+
+let name = "anshul"
+let age = 19
+let isLoggedIn = false
+
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => unique
+//object 
+
+console.log(typeof "undefined") //undefined
+console.log(typeof "null") //object
+
+
+
